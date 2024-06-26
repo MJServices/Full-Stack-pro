f703b54ef8b462c1ca6bb11ac5aaa9b8cb780316
MJServices <mjservices410@gmail.com>
2024-06-26 15:39:36 +0500

    add db connection to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+import { app } from "./app.js";
 import { DBConnection } from "./db/DbConnection.js";
-import express from "express"
-const app = express()
-app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 8000}`)
+DBConnection()
+.then(db => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
 })
-await DBConnection()
+.catch(err => {
+    console.log({status: 500, message: err.message});
+})
