ad288bb24f086cdd6ffdc9c7205c9dc838791ffe
MJServices <mjservices410@gmail.com>
2024-06-26 17:59:39 +0500

    add a custom async handler in utils

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 const app = express()
-app.use(express.json())
+
+app.use(express.json({
+  limit: "16kb",
+}))
+app.use(express.urlencoded({
+  limit: "16kb",
+  extended: true,
+}))
+app.use(cookieParser())
+app.use(express.static("public"))
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}))
 
 app.get("/", (req, res) => {
     res.send("Hello World!")
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import { app } from "./app.js";
 import { DBConnection } from "./db/DbConnection.js";
 
+const port = process.env.PORT || 4000
 
 DBConnection()
 .then(db => {
-    app.listen(process.env.PORT || 4000, () => {
-        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
+    app.on("error", (err) => {
+        console.error({status: 500, message: "Internal Server Error", error: err.message})
+        process.exit(1)
     })
 })
 .catch(err => {
