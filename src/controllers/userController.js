15c3b0c3ba59984b0bcdfe8eb44033a251e71288
MJServices <mjservices410@gmail.com>
2024-06-29 17:11:17 +0500

    all setup configured

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,6 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const registerUser = asyncHandler(async (req, res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+    const {username, email, password} = await req.body
+    res.status(200).json({username, email, password})
 })
