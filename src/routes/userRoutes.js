106d413ad2dbf936be7a06e46af8727270e84517
MJServices <mjservices410@gmail.com>
2024-07-01 19:27:45 +0500

    add refresh token end point

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { loginUser, logoutUser, registerUser } from "../controllers/userController.js"
+import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/userController.js"
 import { upload } from "../middlewares/multerMiddleware.js"
 import { authMiddleware } from "../middlewares/authMiddleware.js"
 const userRouter = Router()
@@ -20,6 +20,6 @@ userRouter.route("/signup").post(
 
 userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(authMiddleware, logoutUser)
-
+userRouter.route("/refreshAccessToken").post(refreshAccessToken)
 
 export default userRouter
